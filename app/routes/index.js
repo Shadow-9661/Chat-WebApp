52175a0245f8d9950e2085fe736ab8b9460648d7
gs-rranjan <rranjan@gainsight.com>
2019-06-04 15:18:36 +0530

    added login

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
-//const login = require('./login')
+const login = require('./login')
 const register = require('./register')
 const router = express.Router();
 
 
 router.use('/register',register);
-//router.use('/login',login);
+router.use('/login',login);
 //router.use('/userhome',user);
 
 module.exports = router;
--- app/routes/login.js
+++ app/routes/login.js
@@ -10,7 +10,7 @@ const db =  knex({
 });
 
 login.post('/',(req,res)=>{
-   db.select('email','hash').from('login').where('email','=',req.body.email)
+   db.select('email','hash').from('account').where('email','=',req.body.email)
         .then(data => {
             const isValid = bcrypt.compareSync(req.body.password,data[0].hash);
             if(isValid){
